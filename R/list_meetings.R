32bb68df76017cb58411f5388ab5f6abc6359c5b
jtrecenti <julio.trecenti@gmail.com>
2021-12-01 21:44:19 -0300

    Simplify list meetings code

--- R/list_meetings.R
+++ R/list_meetings.R
@@ -9,24 +9,21 @@
 #' @export
 list_meetings <- function(user_id) {
   # user_id <- "julio.trecenti@gmail.com"
-
   meetings <- httr::GET(
     paste0("https://api.zoom.us/v2/users/", user_id, "/meetings"),
     httr::add_headers(authorization = paste("Bearer", Sys.getenv("ZOOM_API")))
   )
-
+  # TODO check authentication
   # mensagem <- httr::content(meetings)
 
   existing_meetings <- meetings %>%
-    httr::content() %>%
+    httr::content(simplifyDataFrame = TRUE) %>%
     purrr::pluck("meetings") %>%
-    purrr::map(unlist, recursive = TRUE) %>%
-    purrr::map_dfr(tibble::enframe, .id = "order") %>%
-    tidyr::pivot_wider(
-      names_from = name,
-      values_from = value,
-      names_repair = "unique"
-    )
+    tibble::as_tibble()
+
+  if (nrow(existing_meetings) == 0) {
+    usethis::ui_stop("No meetings found.")
+  }
 
   existing_meetings
 
--- tests/testthat/test-list_meetings.R
+++ tests/testthat/test-list_meetings.R
@@ -3,7 +3,7 @@ test_that("list_meetings works", {
   chamadas_julio <- list_meetings(user_id = "julio.trecenti@gmail.com")
 
   testthat::expect_s3_class(chamadas_julio, "data.frame")
-  testthat::expect_equal(ncol(chamadas_julio), 11)
+  testthat::expect_equal(ncol(chamadas_julio), 10)
 
   #chamadas_bea <- list_meetings(user_id = "milz.bea@gmail.com")
 
